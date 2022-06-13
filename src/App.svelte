e61a422d07d7432c79813a405f22eb3aea2be7b6
jamaps <jeffallen89@gmail.com>
2022-06-13 10:40:32 -0400

    initial commit, add imgs

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+	import TopSofC from './lib/TopSofC.svelte'
+</script>
+
+<style>
+	:root {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+		Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	}
+	:global(body) {
+		padding: 0px;
+		margin: 0px;
+	}
+
+	main {
+		text-align: center;
+		padding: 0px;
+		margin: 0px;
+	}
+
+</style>
+
+<TopSofC/>
+<main>	
+	
+</main>
+
---
+++ src/lib/TopSofC.svelte
@@ -0,0 +1,28 @@
+<script>
+	import logo from '../assets/top-logo.svg'
+</script>
+
+<div id="bar">
+	<a href="https://www.schoolofcities.utoronto.ca/"><img src={logo} alt="School of Cities"></a>
+</div>
+
+<style>
+	#bar {
+		height: 50px;
+		background-color: #121212;
+		margin-bottom: 20px;
+		border-bottom: 1px solid rgb(54, 54, 54);
+		width: 100%;
+		min-width: 500px;
+		padding-left: 10px;
+		padding-right: 10px;
+		text-align: center;
+	}
+	img {
+		color: blue;
+	}
+	img:hover {
+		opacity: 0.7;
+		cursor: pointer;
+	}
+</style>
