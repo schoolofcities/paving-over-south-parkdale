300b089c6accb21159c515497df5b3037664b2b4
jamaps <jeffallen89@gmail.com>
2022-06-13 17:23:03 -0400

    add in globe image, edit some text, some CSS edits

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,20 @@
 <script>
 	import TopSofC from './lib/TopSofC.svelte'
 	import Compare1947v1964 from './lib/Compare1947v1964.svelte'
+	import airphoto1957 from './assets/TA_1957_screenshot.png'
+	import globe1955 from './assets/1955dec1globe.png'
 </script>
 
+<svelte:head>
+	<link href="https://fonts.googleapis.com/css2?family=Bitter&family=Playfair+Display&display=swap" rel="stylesheet"> 
+</svelte:head>
+
 <style>
+	@font-face {
+		font-family: TradeGothicBold;
+		src: url("./assets/Trade Gothic LT Bold.ttf");
+	}
+
 	:root {
 		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
 		Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
@@ -13,18 +24,94 @@
 		margin: 0px;
 	}
 
-	main {
-		text-align: center;
-		padding: 0px;
+	#title {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	#title img {
 		margin: 0px;
+		padding: 0px;
+		max-width: 1000px;
+	}
+	#title h1 {
+		font-family: TradeGothicBold, sans-serif;
+		color: rgb(0, 0, 0);
+		text-align: center;
+		position: absolute;
+		font-size: 44px;
+		max-width: 800px;
+		margin: auto;
+		padding: 10px;
+		background-color: rgba(255, 255, 255, 0.54);
+		border: solid 1px red;
+	}
+
+	.body-text {
+		margin: auto;
+		max-width: 800px;
+		min-width: 300px;
+		padding: 10px;
+		font-size: 21px;
+		font-family: Bitter, serif;
+		line-height: 1.5;
+	}
+
+	.image {
+		margin: auto;
+		max-width: 1000px;
+		border: 1px solid red;
+	}
+	.image img {
+		max-width: 980px;
+		padding: 10px;
+		opacity: 0.65;
 	}
 
 </style>
 
+
+
 <TopSofC/>
-<Compare1947v1964/>
 
-<main>	
+
+<div id="title">
 	
-</main>
+	<img src={airphoto1957} alt="Air Photo 1957">
+	<h1>Paving Over South Parkdale</h1>
+</div>
 
+
+<div class="body-text">
+	<p><b><i>A series of maps documenting the loss of a neighbourhood:</i></b></p>
+	<p>
+	The mid-20th century was an time of contentious highway construction. By
+	
+	In the mid-1950s, Metro Toronto were planning five expressways that connected existing the then-peripheral expressways to the downtown core. 
+	</p>
+	<p>
+	Here's an excerpt from the The Globe and Mail from December 1, 1955, showing their location and priority.
+	</p>
+</div>
+
+<div class="image">
+	<img src={globe1955} alt="Globe and Mail, Dec 1, 1955">
+</div>
+
+<div class="body-text">
+	<p>
+		The two (later renamed after QQQQQ QQQ Gardiner) were 
+
+		The routes of these expressways generally followed greenspace and industrial land, and not through.
+	
+		However, 
+	
+			
+		
+		One 
+	</p>
+</div>
+
+
+
+<Compare1947v1964/>
--- src/lib/Compare1947v1964.svelte
+++ src/lib/Compare1947v1964.svelte
@@ -6,13 +6,13 @@
 
 <div id="compare-images">
 	<div id="years">
-		<div id="left-year">1964</div>
-		<div id="right-year">1947</div>
+		<div id="left-year">1947</div>
+		<div id="right-year">1964</div>
 	</div>
 	<CompareImage
-		imageLeftSrc={img_1964}
+		imageLeftSrc={img_1947}
 		imageLeftAlt="left"
-		imageRightSrc={img_1947}
+		imageRightSrc={img_1964}
 		imageRightAlt="right"
 		--handle-size="3rem"
 		--slider-color="#ffffff"
@@ -28,12 +28,15 @@
 	#left-year {
 		float: left;
 		text-align: left;
+		padding-left: 5px;
 	}
 	#right-year {
 		text-align: right;
+		padding-right: 5px;
 	}
 	#compare-images {
 		margin: auto;
 		max-width: 1000px;
 	}
+	
 </style>
