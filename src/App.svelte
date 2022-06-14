f91f49777379487f47198ac747a91e082e6ca1d3
jamaps <jeffallen89@gmail.com>
2022-06-13 23:15:38 -0400

    add 1979 plans, more text

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import TopSofC from './lib/TopSofC.svelte'
 	import Compare1947v1964 from './lib/Compare1947v1964.svelte'
+	import Compare1979 from './lib/Compare1979.svelte'
 	import airphoto1957 from './assets/TA_1957_screenshot.png'
 	import globe1955 from './assets/1955dec1globe.png'
 </script>
@@ -25,11 +26,15 @@
 	}
 
 	.road {
-		background-color: rgb(247, 247, 247);
+		background-color: rgb(249, 249, 249);
+		margin: auto;
+		width: 100%;
+		padding: 0px;
 	}
 	.white {
 		margin: auto;
-		width: 1400px;
+		max-width: 1400px;
+		width: 100%;
 		background-color: white;
 	}
 
@@ -61,14 +66,22 @@
 
 	.body-text {
 		margin: auto;
-		max-width: 800px;
+		max-width: 700px;
 		min-width: 300px;
-		padding: 10px;
 		padding-top: 25px;
 		padding-bottom: 25px;
 		font-size: 21px;
 		font-family: Bitter, serif;
 		line-height: 1.5;
+	}
+
+	.body-text p {
+		padding: 10px;
+	}
+
+	.body-text .bold {
+		color: red;
+		font-weight: bold;
 	}
 
 	.image {
@@ -91,6 +104,17 @@
 		opacity: 0.89;
 	}
 
+	.info {
+		margin: auto;
+		max-width: 700px;
+		min-width: 300px;
+		padding-top: 25px;
+		padding-bottom: 25px;
+		font-size: 17px;
+		font-family: Bitter, serif;
+		line-height: 1.5;
+	}
+
 </style>
 
 
@@ -110,9 +134,9 @@
 <div class="body-text">
 	<p><b><i>A series of maps documenting the loss of a neighbourhood:</i></b></p>
 	<p>
-	The mid-20th century was an time of contentious highway construction. 
-	There were already several intra-urban expressways in southern Ontario (400, 401, QEW).	
-	By the mid-1950s, Metro Toronto officials were planning five expressways that connected existing the then-peripheral expressways to the downtown core. 
+	The mid-20th century was a time of contentious highway construction.
+
+	By the 1950s, there were already several intra-urban expressways in southern Ontario (Highwys 400, 401, and the QEW) and Metro Toronto officials were planning five additional expressways to connect these existing then-peripheral expressways to the downtown core. 	
 	</p>
 	<p>
 	Here's an excerpt from the The Globe and Mail from December 1, 1955, showing their approximate location and priority.
@@ -129,19 +153,17 @@
 
 <div class="body-text">
 	<p>
-		The two (later renamed after QQQQQ QQQ Gardiner) were 
+		The two top priority highways, the Don Valley Parkway and The Lakeshore Expressway (later renamed after the first chair of the Metro Council, Frederick G. Gardiner) were built during the late 1950s and early 1960s.
 
-		The routes of these expressways generally followed greenspace and industrial land, and not primarilly through residential neighbourhoods.
+		The routes of these expressways generally followed greenspace and industrial land, and not primarilly through residential neighbourhoods. 
 	</p>
 
 	<p>
-		However, there was one neighbourhood that was destroyed during this process, <b>South Parkdale.</b>
-	
+		However, there was one neighbourhood that was destroyed during this process, <span class="bold">South Parkdale</span>, located between the . The neighbourhood primarilly consisted of single-detached homes of similar architectural style to those still found north of the railrod tracks. 
 	</p>
 
 	<p>
-		The following air photos show South Parkdale in 1947, several years before the construction and what the newly finished highway looked like in 1964.
-	
+		The following air photos show South Parkdale in 1947, several years before the construction of the Lakeshore Expressway, and what the newly finished roadway looked like in 1964. The 1947 image also shows how the neighbourhood had already undergone some deformant in the 1920s, when Lakeshore Boulavard was extended through the neighbourhood. By the early 1960s, all houses below the railway were gone, replaced by 12+ lanes of traffic and swaths of manicured grass.
 	</p>
 </div>
 
@@ -149,4 +171,26 @@
 	<div class="white">
 		<Compare1947v1964/>
 	</div>
+</div>
+
+<div class="body-text">	
+	<p>
+		John Sewell, mayor of Toronto from 1978 to 1980, and staff released a plan for covering the expressway to reduce it's noise, better connect Parkdale to the waterfront, and create space for residential development. Due to high costs, the plans never came to fruition, but they do illustrate how the detriments of the expressway could be converted into needed green space and public housing in the future.
+	</p>
+</div>
+
+<div class="road">
+	<div class="white">
+		<Compare1979/>
+	</div>
+</div>
+
+
+
+
+
+
+<div class="info">
+	<p><b>More information and image sources</b></p>
+	<p>Include here!</p>
 </div>
---
+++ src/lib/Compare1979.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { CompareImage } from "svelte-compare-image";
+	import plan_1979_A from '../assets/plan_1979_A.jpg'
+	import plan_1979_B from '../assets/plan_1979_B.jpg'
+</script>
+
+<div id="compare-images">
+	<!-- <div id="years">
+		<div id="left-year">1947</div>
+		<div id="right-year">1964</div>
+	</div> -->
+	<CompareImage
+		imageLeftSrc={plan_1979_A}
+		imageLeftAlt="left"
+		imageRightSrc={plan_1979_B}
+		imageRightAlt="right"
+		--handle-size="3rem"
+		--slider-color="white"
+		--slider-width="0.17rem"
+	/>
+</div>
+
+<style>
+	#years {
+		overflow: hidden;
+		color: red;
+	}
+	#left-year {
+		float: left;
+		text-align: left;
+		padding-left: 5px;
+	}
+	#right-year {
+		text-align: right;
+		padding-right: 5px;
+	}
+	#compare-images {
+		margin: auto;
+		max-width: 1000px;
+	}
+
+</style>
