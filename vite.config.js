eafb7ebd95eb52372c40b887d151455c7a55de8e
Jeff Allen <jeffallen89@gmail.com>
2022-08-25 20:09:26 -0400

    build dist

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/highways-in-south-parkdale",
+  base: "/paving-over-south-parkdale",
   plugins: [svelte()]
 })
